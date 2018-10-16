5231e083a225aaee178279b213e76db7badf06e1
Robert Leo Masti <rlm7819@nrlogin2.cluster>
2018-10-16 18:05:19 -0400

    Finally CUDA WORKS

--- parrGpu/test/occaAddVector.cpp
+++ parrGpu/test/occaAddVector.cpp
@@ -3,13 +3,21 @@
 int main(int argc, const char **argv){
 
   //occa::setDevice((std::string) args["options/device"]);
-  occa::setDevice("mode: 'Serial'");
+  occa::setDevice("mode: 'CUDA', "
+          "device_id : 0");
+  //occa::setDevice("mode: 'Serial'");
 
   int entries = 5;
 
+  /*
   float *a = new float[entries];
   float *b = new float[entries];
   float *ab = new float[entries];
+  */
+
+  float *a  = (float*) occa::umalloc(entries * sizeof(float));
+  float *b  = (float*) occa::umalloc(entries * sizeof(float));
+  float *ab = (float*) occa::umalloc(entries * sizeof(float)); 
 
   for(int i = 0; i < entries; i++){
     a[i] = i;
