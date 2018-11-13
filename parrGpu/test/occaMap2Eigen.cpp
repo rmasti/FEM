42d2c9fc2d6b70f9c7ca01973f7956c88be151c1
Robert Leo Masti <rlm7819@nrlogin2.cluster>
2018-11-13 15:15:14 -0500

    added occa

---
+++ parrGpu/test/occaMap2Eigen.cpp
@@ -0,0 +1,35 @@
+#include "mhdRT.hpp"
+#include "occa.hpp"
+
+int main(int argc, const char **argv){
+
+  //occa::setDevice("mode: 'CUDA', "
+   //       "device_id : 0");
+  occa::device device("mode: 'Serial'");
+          //"device_id : 0");
+
+  int nrows = 3;
+  int ncols = 5;
+
+
+  Map2Eigen *U = new Map2Eigen(nrows, ncols, NEQ);
+  for(int j = 0; j < nrows; j++)
+    for(int i = 0; i < ncols; i++)
+      for(int eq = 0; eq < NEQ; eq++)
+        U->Q_raw[(j*ncols+i)*NEQ+eq] = double((j*ncols+i)*NEQ+eq);
+
+  cout << U->Q[rhoid] << endl;
+ 
+  occa::memory o_U = device.malloc(nrows*ncols*NEQ*sizeof(double), U->Q_raw);
+
+  occa::kernel occaMap2Eigen = occa::buildKernel("test/occaMap2Eigen.okl", "occaMap2Eigen");
+
+  occaMap2Eigen(nrows, ncols, o_U);
+  
+  o_U.copyTo(U->Q_raw);
+
+  cout << U->Q[rhoid] << endl;
+  cout << U->Q[uid] << endl;
+
+  return 0;
+}
