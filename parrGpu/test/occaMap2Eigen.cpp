6cb23935a5ac1bd41586f70d2892274ca68e907f
Robert Leo Masti <rlm7819@nrlogin2.cluster>
2018-11-27 09:16:10 -0500

    added to the test function

--- parrGpu/test/occaMap2Eigen.cpp
+++ parrGpu/test/occaMap2Eigen.cpp
@@ -1,18 +1,17 @@
 #include "mhdRT.hpp"
 #include "occa.hpp"
 
+//using namespace occa;
+
 int main(int argc, const char **argv){
 
-  //occa::setDevice("mode: 'CUDA', "
-   //       "device_id : 0");
-  //occa::device device("mode: 'Serial'");
   occa::device device("mode: 'CUDA', device_id : 0");
 
   int nrows = 3;
   int ncols = 5;
 
-
   Map2Eigen *U = new Map2Eigen(nrows, ncols, NEQ);
+  double* other = new double[nrows*ncols*NEQ];
   for(int j = 0; j < nrows; j++)
     for(int i = 0; i < ncols; i++)
       for(int eq = 0; eq < NEQ; eq++)
@@ -20,23 +19,17 @@ int main(int argc, const char **argv){
 
   cout << U->Q[rhoid] << endl;
  
-  occa::memory o_U = device.malloc(nrows*ncols*NEQ*sizeof(double), U->Q_raw);
-  //cout << nrows*ncols*NEQ*sizeof(RowMajorMatrixXd) << endl;
-  //occa::memory o_U = device.malloc(sizeof(Eigen::MatrixXd)*nrows*ncols, U->Q[rhoid].data());
-
-  occa::properties props;// = occa::deviceProperties();
-
+  occa::memory o_U = device.malloc(nrows*ncols*NEQ*sizeof(double*), U->Q_raw);
+  occa::properties props;
   props["defines/NEQ"] = NEQ;
 
   occa::kernel occaMap2Eigen = occa::buildKernel("test/occaMap2Eigen.okl", "occaMap2Eigen", props);
 
   occaMap2Eigen(nrows, ncols, o_U);
-
-  cout << "\n ________________ MADE IT HERE ____________________\n" << endl;
   
-  o_U.copyTo(U->Q_raw);
-
   device.finish();
+
+  o_U.copyTo(U->Q_raw);
 
   cout << U->Q[rhoid] << endl;
 
